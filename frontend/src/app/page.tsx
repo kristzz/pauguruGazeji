92c55847239abaebfd639a89ee979d650722f54b
dpguntisg <crazymangaming111@gmail.com>
2024-09-09 12:53:42 +0300

    testins

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
       <h2 className="bg-main-blue">blue</h2>
       <h2 className="bg-main-white">white</h2>
       <h2 className="bg-main-red">red</h2>
+      <h1>man patik olass</h1>
     </main>
   );
 }
