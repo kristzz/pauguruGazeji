0c477f599f391a26f3f6e8c515c3b4a92aac87bc
21DP2JFern <12djfernats@rvt.lv>
2024-09-09 14:41:31 +0300

    poga

---
+++ frontend/src/app/about-you/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div className=" flex flex-col items-center mt-16">
+        <p className="text-main-blue text-2xl w-52 text-center ">Specify the subjects you want to study</p>
+        <button className="bg-gray-300 border-main-blue border-2 rounded-lg w-12">Math</button>
+      </div>
+      
+    );
+  }
