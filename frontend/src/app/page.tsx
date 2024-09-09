345175ad00e1913fe2e467da2133379ea1a9dddc
kristzz <emils.krists@gmail.com>
2024-09-09 14:21:00 +0300

    settings initial commit

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <h1>pauguru gazeji</h1>
-      <h2 className="bg-main-blue">blue</h2>
+      <h2 className="bg-main-blue">blue</h2>    
       <h2 className="bg-main-white">white</h2>
       <h2 className="bg-main-red">red</h2>
     </main>
---
+++ frontend/src/app/settings/page.tsx
@@ -0,0 +1,8 @@
+export default function Settings() {
+    return (
+      <main>
+        <h1>Settings Page</h1>
+        <p>Here you can configure your settings.</p>
+      </main>
+    );
+  }
