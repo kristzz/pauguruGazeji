e0d10f0d05b68ccd7cf12da17c175ed22d83f64d
dpguntisg <crazymangaming111@gmail.com>
2024-09-10 13:51:58 +0300

    640 width landing page media query

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -13,19 +14,26 @@ export default function Home() {
       <div className="flex flex-col items-center h-screen">
 
         <div className="flex flex-row">
-          <div className=" bg-main-red w-44 h-16 rounded-b-xl z-0"></div>
-          <div className="absolute bg-main-blue w-12 h-56 rounded-s-xl right-0 top-64"></div>
-          <div className="absolute bg-main-blue w-16 h-36 rounded-e-xl left-0 top-52"></div>
-          <div className="absolute bg-main-red w-8 h-28 rounded-e-xl left-0 bottom-36"></div>
-          <div className="absolute bg-main-blue w-40 h-16 rounded-tr-xl bottom-0 left-0"></div>
-          <div className="absolute bg-main-red w-20 h-24 rounded-tl-xl bottom-0 right-0"></div>
-
-
+          <div className=" bg-main-red w-60 h-16 rounded-b-xl z-0
+                          sm:w-72 sm:h-24"></div>
+          <div className="absolute bg-main-blue w-14 h-60 rounded-s-xl right-0 top-64
+                          sm:w-24 sm:h-72"></div>
+          <div className="absolute bg-main-blue w-16 h-36 rounded-e-xl left-0 top-52
+                          sm:w-20 sm:h-64"></div>
+          <div className="absolute bg-main-red w-12 h-32 rounded-e-xl left-0 bottom-36
+                          sm:w-16 sm:h-40 sm:bottom-44"></div>
+          <div className="absolute bg-main-blue w-40 h-16 rounded-tr-xl bottom-0 left-0
+                          sm:w-48 sm:h-20"></div>
+          <div className="absolute bg-main-red w-32 h-24 rounded-tl-xl bottom-0 right-0
+                          sm:w-40 sm:h-32"></div>
         </div>
 
-        <h1 className="text-main-blue text-2xl text-center w-52 mt-16 mb-44 z-1" >Start your learning journey today!</h1>
-        <button className="bg-main-blue text-main-white text-lg rounded-lg h-12 w-40 my-4">Register</button>
-        <button className="bg-main-red text-main-white text-lg rounded-lg h-12 w-40 my-4">Sign in</button>
+        <h1 className="text-main-blue text-2xl text-center w-[50%] mt-16 mb-44 z-1
+                      sm:mt-56 sm:text-4xl" >Start your learning journey today!</h1>
+        <Link href="/register"><button className="bg-main-blue text-main-white text-lg rounded-lg h-12 w-40 my-4
+                                                  sm:w-60 sm:h-16 sm:text-xl">Register</button></Link>
+        <Link href="/signin"><button className="bg-main-red text-main-white text-lg rounded-lg h-12 w-40 my-4
+                                                  sm:w-60 sm:h-16 sm:text-xl">Sign In</button></Link>
       </div>
     </main>
   );
--- frontend/src/app/signin/page.tsx
+++ frontend/src/app/signin/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Signin() {
     return(
 
@@ -9,10 +11,10 @@ export default function Signin() {
                     <input className="h-12 w-64 mt-24 mb-4 rounded-lg p-4" type="email" id="email" name="email" placeholder="Enter your email"/>
                     <input className="h-12 w-64 my-4 rounded-lg p-4" type="password" id="password" name="password" placeholder="Password"/>
                     <div className="mb-4">
-                        <a className="text-main-blue" href="">Forgot Password?</a>
+                       <Link href="/forgot"> <a className="text-main-blue" href="">Forgot Password?</a> </Link>
                     </div>
                     <div className="flex items-center mt-8">
-                        <button className="bg-main-blue text-main-white text-lg rounded-lg h-12 w-64">sign in</button>
+                        <button className="bg-main-blue text-main-white text-lg rounded-lg h-12 w-64">Sign in</button>
                     </div>
                 </form>
             </div>
