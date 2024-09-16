46f5a7c5f0b6bd16615ad62403297eb9c015a6f7
kristzz <emils.krists@gmail.com>
2024-09-16 11:23:36 +0300

    aaaaaaaaa

--- frontend/src/app/settings/page.tsx
+++ frontend/src/app/settings/page.tsx
@@ -1,8 +1,25 @@
 export default function Settings() {
     return (
       <main>
-        <h1>Settings Page</h1>
-        <p>Here you can configure your settings.</p>
+        <SettingsButton label="Notifications" />
+        <SettingsButton label="Account privacy" />
+        <SettingsButton label="Subscriptions" />
+        <SettingsButton label="Languages" />
+        <SettingsButton label="Help" />
+        <SettingsButton label="Account" />
+        <button className="bg-main-red">!Log out!</button>
       </main>
     );
   }
+
+type SettingsButtonProps = {
+  label: string;
+};
+
+function SettingsButton({label}: SettingsButtonProps) {
+  return(
+    <button className="bg-main-white">
+      {label}
+    </button>
+  )
+}
