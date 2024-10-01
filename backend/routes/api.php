67f1b8404c3f5c17adc44bc12f3121bf6a726106
dpguntisg <crazymangaming111@gmail.com>
2024-10-01 09:20:02 +0300

    Routes working, added a random ahh file Routeserviceprovider

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\AuthController;
 
-Route::post('/register', [UserController::class, 'register']);
-Route::post('/login', [UserController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
 
