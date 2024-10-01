d5291fd30cf32e4463f610db6c76e8263f9bf51c
Gloopjuice <girts.gagainis@gmail.com>
2024-10-01 09:37:37 +0300

    hello

--- backend/app/Http/Controllers/Api/UserController.php
+++ backend/app/Http/Controllers/Api/UserController.php
@@ -7,5 +7,17 @@ use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    //
+    public function sendMessage(Request $request)
+    {
+        $request->validate([
+            'userID'
+        ])
+            
+        messages::create([
+            'id' => $request->id,
+            'content' => $request->content,
+            'created_at'=>$date,
+            'updated_at'=>$date
+        ])
+    }
 }
--- backend/app/Models/Message.php
+++ backend/app/Models/Message.php
@@ -25,3 +25,4 @@ class Message extends Model
         return $this->belongsTo(User::class);
     }
 }
+
