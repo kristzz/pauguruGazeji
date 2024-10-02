b9f88e017fbee216638bb034421564765f0b5133
21DP2RJerm <12DRJermacans@rvt.lv>
2024-10-02 12:14:51 +0300

    nelielas izmainas profila lapai

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -4,8 +4,10 @@ export default function Profile() {
             <div className="bg-main-white h-screen w-screen flex flex-col items-center relative">
                 <div className="bg-main-blue w-screen h-[50%] absolute inset-x-0 bottom-0"></div>
                 <div className="bg-main-white w-[50%] h-[50%] shadow-2xl relative top-[20%] rounded-lg align-center border-2 border-main-red">
-                    <div className="text-main-red top-10 relative text-2xl text-center">Balderjanis</div>
-                    <div className="text-main-red top-10 relative text-lg text-center">Balderjanis@yandex.ru</div>
+                    <div className="text-main-red mt-5 relative text-2xl text-center">Balderjanis</div>
+                    <div className="text-main-red mt-5 relative text-lg text-center">Balderjanis@yandex.ru</div>
+                    <div className="text-main-red mt-3 relative text-lg text-center">High School Education</div>
+                    <div className="text-main-red mt-3 relative text-lg text-center">Points: 204</div>
                     <div className="grid grid-cols-3 place-items-center absolute w-full inset-x-0 bottom-14">
                         <div className="bg-main-red w-16 h-16 rounded-lg flex items-center justify-center">
                         </div>
