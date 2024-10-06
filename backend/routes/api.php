771864b9f55ab912d6bf63a05d946867798dd5d9
Gloopjuice <girts.gagainis@gmail.com>
2024-10-07 01:38:15 +0300

    ups

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,9 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\MessageController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-
-
+Route::post('/postMessage', [MessageController::class, 'postMessage']);
+Route::get('/getMessage', [MessageController::class, 'getMessage']);
+Route::get('/getMessageFrom', [MessageController::class, 'getMessageFrom']);
