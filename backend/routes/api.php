36a364a612e1f0f6d5a2b606af73fd848b6af9dc
Gloopjuice <girts.gagainis@gmail.com>
2024-10-07 01:23:03 +0300

    big commit, kkas iet

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\MessageController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -14,4 +15,6 @@ Route::get('profile', [AuthController::class, 'profile']);
 
 });
 
-
+Route::post('/postMessage', [MessageController::class, 'postMessage']);
+Route::get('/getMessage', [MessageController::class, 'getMessage']);
+Route::get('/getMessageFrom', [MessageController::class, 'getMessageFrom']);
