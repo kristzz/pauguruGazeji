a67b76ca76c70fc873be1fee28b314e5c2fffeec
kristzz <emils.krists@gmail.com>
2024-10-20 13:40:02 +0300

    adwa

--- frontend/src/app/settings/languages/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Settings() {
-    return (
-      <main>
-        <h3>Language support</h3>
-
-        <p>Language support to be added</p>
-      </main>
-    );
-}
--- frontend/src/app/settings/layout.tsx
+++ frontend/src/app/settings/layout.tsx
@@ -26,14 +26,6 @@ export default function SettingsLayout({ children }: { children: ReactNode }) {
             </Link>
           </li>
           <li className="mb-3">
-            <Link href="/settings/languages" className="flex space-x-2">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m10.5 21 5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 0 1 6-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 0 1-3.827-5.802" />
-                </svg>
-                <p>Languages</p>
-            </Link>
-          </li>
-          <li className="mb-3">
             <Link href="/settings/notifications" className="flex space-x-2">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
