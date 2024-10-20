03f12001f1c7ad7b1a690e33938784e6635fc12c
21DP2JFern <12djfernats@rvt.lv>
2024-10-20 15:30:49 +0300

    aaa

---
+++ frontend/src/app/certificates/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export default function Profile() {
+    
+    
+    return (
+        <main>
+
+        </main>
+    );
+}
